e315933fa17aa3533307ca103b0f952840c1e867
unknown <ngocduc.2109@gmail.com>
2021-04-02 14:34:49 +0700

    [Thực hành] Tính số ngày trong tháng

---
+++ src/DaysInMonth.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+public class DaysInMonth {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Which month that you want to count days? ");
+        int month = scanner.nextInt();
+        String days = "";
+        switch (month){
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                days = "31";
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                days = "30";
+                break;
+            case 2:
+                days = "28 or 29";
+                break;
+        }
+        if (days != "") System.out.printf("The month '%d' has %s days!", month, days);
+        else System.out.print("Invalid input!");
+    }
+}
